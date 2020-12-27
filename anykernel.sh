788fbf415a8f5b8bbf6d1ad8b96d1ba17a920841
DhruvChhura <dhruvchhura001@gmail.com>
2020-12-27 19:52:38 +0530

    changed name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Darkonah Kernel   Telegram - @DhruvChhura
+kernel.string=Ryzo Kernel   Telegram - @DhruvChhura
 do.devicecheck=0
 do.modules=0
 do.systemless=1
